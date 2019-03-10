eb2c81245dff2060112b794ed23f2fb1aaf92b4c
Brian Simonton <bsimonton78@gmail.com>
2019-03-10 00:15:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -13,7 +13,17 @@ class Owner
     return "I am a #{@species}."
   end
   
-  
+   def buy_fish(name_of_fish)
+    @pets[:fishes] << Fish.new(name_of_fish)
+  end
+
+  def buy_dog(name_of_dog)
+    @pets[:dogs] << Dog.new(name_of_dog)
+  end
+
+  def buy_cat(name_of_cat)
+    @pets[:cats] << Cat.new(name_of_cat)
+  end
   
   
   
