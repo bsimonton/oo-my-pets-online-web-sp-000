4e7654b65f6dc1c844969959178282a634cf383a
Brian Simonton <bsimonton78@gmail.com>
2019-03-10 00:13:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,7 +3,11 @@ class Owner
   attr_accessor :name, :pets
   attr_reader :species
   
-  
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
   
   
   
